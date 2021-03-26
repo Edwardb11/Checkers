5ede2e8cb34e39525aeb62dc73619e3c93a38c33
Edward Brito <Edwardbrito11@hotmail.com>
2021-03-26 16:29:08 -0400

    Ok

--- piece.py
+++ piece.py
@@ -7,9 +7,9 @@ class Piece():
 
     def promote(self):
         self.isKing |= True
-        
+
 # | = performs an in-place + operation between pairs of objects. In particular, between:
-      #joints: a union operation
-      #dicta: an update operation
-      #counters: a join operation (multigame)
+      # joints: a union operation
+      # dicta: an update operation
+      # counters: a join operation (multigame)
       # numbers: a bitwise OR, binary operation
