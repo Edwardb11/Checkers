1343163f21d4602d43ddc41f5aeda9f544c71376
Edward Brito <Edwardbrito11@hotmail.com>
2021-03-26 15:54:32 -0400

    line 14 in comment

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from board import *
+
+
 class App(tk.Tk):
     def __init__(self, *args, **kwargs):
         tk.Tk.__init__(self, *args, **kwargs)
-        self.canvas = tk.Canvas(self, width=640, height=640, borderwidth=0, highlightthickness=0)
+        self.canvas = tk.Canvas(
+            self, width=640, height=640, borderwidth=0, highlightthickness=0)
         self.canvas.pack(side="top", fill="both", expand="true")
         self.Game = Game()
         self.rows = 8
@@ -11,7 +14,7 @@ class App(tk.Tk):
         self.canvas.bind("<Configure>", self.redraw)
         self.status = tk.Label(self, anchor="w")
         self.status.pack(side="bottom", fill="x")
-        self.tile_colors = ['white', 'black']
+        #self.tile_colors = ['white', 'black']
 
     def redraw(self, event=None):
         self.canvas.delete("rect")
@@ -23,13 +26,18 @@ class App(tk.Tk):
                 y1 = row * cellheight
                 x2 = x1 + cellwidth
                 y2 = y1 + cellheight
-                tile.gui_tile = self.canvas.create_rectangle(x1,y1,x2,y2, fill=tile.color, tags="rect")
+                tile.gui_tile = self.canvas.create_rectangle(
+                    x1, y1, x2, y2, fill=tile.color, tags="rect")
                 if tile.isOccupied():
                     if tile.piece.isKing:
-                        oval = self.canvas.create_oval(x1,y1,x2,y2, fill=tile.piece.color, width = 10)
-                        self.canvas.tag_bind(oval, "<1>", lambda event, row=row, column=column: self.clicked(row, column))
+                        oval = self.canvas.create_oval(
+                            x1, y1, x2, y2, fill=tile.piece.color, width=10)
+                        self.canvas.tag_bind(
+                            oval, "<1>", lambda event, row=row, column=column: self.clicked(row, column))
                     else:
-                        oval = self.canvas.create_oval(x1,y1,x2,y2, fill=tile.piece.color)
-                        self.canvas.tag_bind(oval, "<1>", lambda event, row=row, column=column: self.clicked(row, column))
-                self.canvas.tag_bind(tile.gui_tile, "<1>", lambda event, row=row, column=column: self.clicked(row, column))
-            
+                        oval = self.canvas.create_oval(
+                            x1, y1, x2, y2, fill=tile.piece.color)
+                        self.canvas.tag_bind(
+                            oval, "<1>", lambda event, row=row, column=column: self.clicked(row, column))
+                self.canvas.tag_bind(tile.gui_tile, "<1>", lambda event,
+                                     row=row, column=column: self.clicked(row, column))
