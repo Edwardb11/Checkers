2d126355386f16337668aee3421518cee7642ad8
Edward Brito <Edwardbrito11@hotmail.com>
2021-05-31 13:45:19 -0400

    abstract function, comments, renaming to move function

--- board.py
+++ board.py
@@ -3,10 +3,9 @@ from piece import *
 from tile import *
 from main import *
 
-
 class Game():
     def __init__(self):
-        self.players = ['red', 'yellow']
+        self.players = ['tan2', 'LightBlue1']
         self.tile_colors = ['white', 'black']
 #         self.num_pieces = {color:12 for color in self.players}
         self.turn = 0
@@ -19,10 +18,10 @@ class Game():
                                self.tile_colors[(row_idx + col_idx) % 2])
                 if newTile.color == self.tile_colors[1]:
                     if row_idx in range(0, 3):
-                        newPiece = Piece("yellow", row_idx, col_idx)
+                        newPiece = Piece("LightBlue1", row_idx, col_idx)
                         newTile.addPiece(newPiece)
                     if row_idx in range(5, 8):
-                        newPiece = Piece("red", row_idx, col_idx)
+                        newPiece = Piece("tan2", row_idx, col_idx)
                         newTile.addPiece(newPiece)
                 row.append(newTile)
             self.Board.append(row)
@@ -43,39 +42,42 @@ class Game():
                 self.selected = (None, None)
         return False
 
-    def move(self, row, col):
+    #rename to move_tile
+    def move_tile(self, row, col):
         assert self.isSelected()
         cur_row = self.selected[0]
         cur_col = self.selected[1]
         piece = self.Board[cur_row][cur_col].piece
+
+        #Abstract function
+        def  max_valid(x,y):
+            return [(cur_row + i, cur_col + j)
+                for i in x for j in y]
+
         if piece.isKing:
-            max_valid_moves = [(cur_row + i, cur_col + j)
-                               for i in [-1, 1] for j in [-1, 1]]
-            max_valid_attacks = [(cur_row + i, cur_col + j)
-                                 for i in [-2, 2] for j in [-2, 2]]
+            max_valid_moves = max_valid([-1,1],[-1,1])
+            max_valid_attacks = max_valid([-2,2],[-2,2])
         else:
             if self.turn == 0:
-                max_valid_moves = [(cur_row + i, cur_col + j)
-                                   for i in [-1] for j in [-1, 1]]
-                max_valid_attacks = [(cur_row + i, cur_col + j)
-                                     for i in [-2] for j in [-2, 2]]
+                max_valid_moves = max_valid([-1],[-1,1])
+                max_valid_attacks = max_valid([-2],[-2,2])
             else:
-                max_valid_moves = [(cur_row + i, cur_col + j)
-                                   for i in [1] for j in [-1, 1]]
-                max_valid_attacks = [(cur_row + i, cur_col + j)
-                                     for i in [2] for j in [-2, 2]]
+                max_valid_moves = max_valid([1],[-1,1])
+                max_valid_attacks = max_valid([2],[-2,2])
 
+        #max_valid_moves -> basically here valid maximum movements are controlled
+        #max_valid_attacks -> here the maximum valid attacks are controlled
         max_valid_moves = [pos for pos in max_valid_moves if pos[0] in range(
             8) and pos[1] in range(8)]
         max_valid_attacks = [
             pos for pos in max_valid_attacks if pos[0] in range(8) and pos[1] in range(8)]
-
         max_valid_moves = [
             pos for pos in max_valid_moves if not self.Board[pos[0]][pos[1]].isOccupied()]
         max_valid_attacks = [
             pos for pos in max_valid_attacks if not self.Board[pos[0]][pos[1]].isOccupied()]
-
+        
         new_valid = []
+        #scan to see if it is busy maximum valid attacks
         for pos in max_valid_attacks:
             middle = self.Board[int((cur_row + pos[0])//2)
                                 ][int((cur_col + pos[1])//2)]
@@ -83,6 +85,7 @@ class Game():
                 new_valid.append(pos)
         max_valid_attacks = new_valid
 
+        #Compare if it can be moved can be moved without killing or killing the piece
         if (row, col) in max_valid_moves:
             moved_piece = self.Board[cur_row][cur_col].removePiece()
             moved_piece.row = row
